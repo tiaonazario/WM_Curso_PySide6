77e0d1057edbc1c63e118e1e38cb4520dac8337c
Tião Nazário <tiaogogo@gmail.com>
2021-07-31 16:43:02 -0300

    a

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hoje")
+# IMPORT MODULES
+import os
+import sys
+# IMPORT qt_core
+from qt_core import *
+# IMPORT MAIN WINDOW
+from gui.windows.main_window.ui_main_window import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
